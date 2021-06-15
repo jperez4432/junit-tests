eac3e413d2ca35fcc1ed52de09aa5ce1063e8d84
Juan Perez <jperez4432@gmail.com>
2021-06-15 10:30:29 -0500

    first test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,2 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class HelloWorldTest {
+    @Test
+    public void testIfHelloWorldWorks() {
+        String expected = "Hello World!";
+
+        assertEquals(expected, HelloWorld.hello());
+    }
 }
